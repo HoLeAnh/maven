4a24d1acd386e1ab5c3881e16d0c293c3ad6e5af
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-09-22 18:16:44 +0700

    sang1-3

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("4");
-        System.out.println("SANG RICH");
+        System.out.println("SANG RICH LAM");
     }
 }
